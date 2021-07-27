c8ce90c2596d6d67e63f259d07453c787a967f94
Tomas Lanterna <tomas.lanterna@peya.local>
2021-07-27 17:44:31 -0300

    navBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar';
+import Accordion from '@material-ui/core/Accordion';
+import { Toolbar, IconButton, Typography, Button, MenuItem,Menu } from '@material-ui/core';
+import {MenuIcon} from "@material-ui/icons/Menu";
+import * as Mui from '@material-ui/core';
+
+
+
+
+
+function NavBar() {
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton>
+                    <Menu/>
+                </IconButton>
+                <Typography variant="h6" /*className={classes.title}*/>
+                    Tienda Online
+                </Typography>
+                <Button color="inherit">Login</Button>
+                <Button color="inherit">Catalogo de Productos</Button>
+                <Button color="inherit">Mis Compras</Button>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default NavBar
