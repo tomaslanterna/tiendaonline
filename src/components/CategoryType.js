186d98394462da72239e645c774108175c12d76f
tomas lanterna <tomas.lanterna@hotmail.com>
2021-10-01 21:30:37 -0300

    tiendaonline-2.4v

--- src/components/CategoryType.js
+++ src/components/CategoryType.js
@@ -2,26 +2,34 @@ import React from 'react'
 import { NavLink } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
-
-    root: {
-
-        borderRadius:80,
-        marginTop:10
-
-    }
 
 
-}));
+const CategoryType = ({ img, id }) => {
+    const useStyles = makeStyles((theme) => ({
 
-const CategoryType=({ img, id })=>{
+        root: {
+            marginTop: 10
+        },
+        fondo:{
+            backgroundImage: `url(${img})`,
+            backgroundSize:'cover',
+            backgroundPositionY:'center',
+            borderRadius: 50,
+            width:'100%',
+            height:300,
+            marginTop: 10
+        }
+
+
+    }));
+
     const classes = useStyles();
     return (
-        <div>
-            <NavLink to={{ pathname: "/category/" + id }}>         
-                <img src={img} alt="Category" className={classes.root} width={800} height={350}/>                
-            </NavLink>
-        </div>
+        <NavLink to={{ pathname: "/category/" + id }}>
+            <div className={classes.fondo}>
+            </div>
+            {/*<img src={img} alt="Category" className={classes.root} width={800} height={350} />*/}
+        </NavLink>
     )
 }
 
--- src/components/CategorysContainer.js
+++ src/components/CategorysContainer.js
@@ -14,9 +14,9 @@ const CategorysContainer=()=>{
     return (
         <div>
             <Grid container className={classes.root}>
-                <Grid xs={8}><CategoryType img={"https://firebasestorage.googleapis.com/v0/b/tiendaonline-ad1a6.appspot.com/o/TECNOLOGIA.jpg?alt=media&token=1a838246-875c-4bfb-8308-511356046e84"} id={"1"}/></Grid>
-                <Grid xs={8}><CategoryType img={"https://firebasestorage.googleapis.com/v0/b/tiendaonline-ad1a6.appspot.com/o/SportWear.jpg?alt=media&token=2b0f0782-e229-4560-8746-3f369db36cfa"} id={"2"}/></Grid>
-                <Grid xs={8}><CategoryType img={"https://firebasestorage.googleapis.com/v0/b/tiendaonline-ad1a6.appspot.com/o/UrbanWear.jpg?alt=media&token=68ada30b-fd17-4e5f-987a-81ec8d8e6c98"} id={"3"}/></Grid>
+                <Grid xs={12}><CategoryType img={"https://firebasestorage.googleapis.com/v0/b/tiendaonline-ad1a6.appspot.com/o/TECNOLOGIA.jpg?alt=media&token=1a838246-875c-4bfb-8308-511356046e84"} id={"1"}/></Grid>
+                <Grid xs={12}><CategoryType img={"https://firebasestorage.googleapis.com/v0/b/tiendaonline-ad1a6.appspot.com/o/SportWear.jpg?alt=media&token=2b0f0782-e229-4560-8746-3f369db36cfa"} id={"2"}/></Grid>
+                <Grid xs={12}><CategoryType img={"https://firebasestorage.googleapis.com/v0/b/tiendaonline-ad1a6.appspot.com/o/UrbanWear.jpg?alt=media&token=68ada30b-fd17-4e5f-987a-81ec8d8e6c98"} id={"3"}/></Grid>
             </Grid>
         </div>
     )
